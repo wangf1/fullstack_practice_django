ae9470f3ffe3bc95c3dfd8d53b90d423452d8559
Mark Wang <armstrong.wang2000@gmail.com>
2024-06-19 18:32:07 -0600

    How django url mapping works

--- fullstack_practice/api/views.py
+++ fullstack_practice/api/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def main(request):
+    return HttpResponse("Hello, world. You're at the api index.")
